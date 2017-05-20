51a5f165c1a255468830d5f9e405d9118bc0e5ce
tibing <parapupum@gmail.com>
2017-05-20 17:12:43 +0300

    refactor(simple-replacement): use alphabet from config

--- src/simple-replacement/simple-replacement.test.js
+++ src/simple-replacement/simple-replacement.test.js
@@ -1,4 +1,5 @@
 const simple_replacement = require('./simple-replacement');
+const config = require('./config');
 
 describe('Simple replacement code should encrypt correctly', () => {
   test('correctly encrypt my text', () => {
@@ -9,48 +10,43 @@ describe('Simple replacement code should encrypt correctly', () => {
   });
   
   test('correctly encrypt Halezof text', () => {
-    const alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
     const key = 'ЦВЮЕНПЛЖЭКЩАЪУЙЯОБГЗЁДФЬИХЫЧСРМТШ';
     const text = 'ИНДОНЕЗИЯ';
     const result = 'КЙНЯЙПЭКШ';
     
-    expect(simple_replacement(text, alphabet, key)).toBe(result);
+    expect(simple_replacement(text, config.alphabet, key)).toBe(result);
   });
   
   test('correctly encrypt Politay text', () => {
-    const alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
     const key = 'КПЦОАЕНЗИРУЬГЙЖШВЛЭЩФМЫТДСБЁЮХЧЪЯ';
     const text = 'ГОНКОНГ';
     const result = 'ОШЖЬШЖО';
     
-    expect(simple_replacement(text, alphabet, key)).toBe(result);
+    expect(simple_replacement(text, config.alphabet, key)).toBe(result);
   });
   
   test('correctly encrypt Drozd text', () => {
-    const alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
     const key = 'ХРЬБЁКШЩУЯЙМЕЛЖФИЧВЫТСЮПНЭАЗОГДЦЪ';
     const text = 'ДОМИНИКА';
     const result = 'ЁФЛЯЖЯМХ';
     
-    expect(simple_replacement(text, alphabet, key)).toBe(result);
+    expect(simple_replacement(text, config.alphabet, key)).toBe(result);
   });
   
   test('correctly encrypt Zenkevich text', () => {
-    const alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
     const key = 'ЖЧЮАНДМФЦЛЕЯЙОРЪЩИЬШЁЗЭКТБВЫГХСУП';
     const text = 'ДЖИБУТИ';
     const result = 'НФЛЧЁШЛ';
     
-    expect(simple_replacement(text, alphabet, key)).toBe(result);
+    expect(simple_replacement(text, config.alphabet, key)).toBe(result);
   });
   
   test('correctly encrypt Aliluiko text', () => {
-    const alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
     const key = 'ЕНТЯХШРЩЬБВОГЧЗЛУДЪЮЭМКЁЙЦСИЫФАПЖ';
     const text = 'ИЗРАИЛЬ';
     const result = 'БЬДЕБГФ';
     
-    expect(simple_replacement(text, alphabet, key)).toBe(result);
+    expect(simple_replacement(text, config.alphabet, key)).toBe(result);
   });
 });
 
